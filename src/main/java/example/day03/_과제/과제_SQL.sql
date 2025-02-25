da8198dc7da449c9b5105cd8bb95cd2aa9e797ca
je-young <jey2965@gmail.com>
2025-02-26 02:58:53 +0900

    DAY04 실습.txt

--- src/main/java/example/day03/_과제/과제_SQL.sql
+++ src/main/java/example/day03/_과제/과제_SQL.sql
@@ -1,8 +1,19 @@
 drop database if exists springweb;
 create database springweb;
 use springweb;
-create table board (
-bno int auto_increment primary key,
-title varchar(255) not null,
-content text not null
+
+create table board
+(
+    bno     int auto_increment primary key,
+    title   varchar(255) not null,
+    content text         not null
+);
+
+create table student
+(
+    sno  int auto_increment,
+    name varchar(100),
+    kor  int,
+    math int,
+    constraint primary key (sno)
 );
