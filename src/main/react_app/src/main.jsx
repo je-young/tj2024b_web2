c41eb038c0a74ad519f3119414f5de9b62596dc0
je-young <jey2965@gmail.com>
2025-03-05 13:56:21 +0900

    React Component & JSX

--- src/main/react_app/src/main.jsx
+++ src/main/react_app/src/main.jsx
@@ -16,8 +16,20 @@ const root = createRoot(document.querySelector('#root'));
 // day01 : '컴포넌트 만들기' 렌더링
 // import 호출할 컴포넌트명 from '경로/파일명(확장자)';
 import Component1 from './example/1_동작구조/Component1.jsx';
-root.render(<Component1 />);
+//root.render(<Component1 />);
 
 // day01 : 여러 컴포넌트 만들기
 import Component2 from './example/1_동작구조/Component2.jsx';
-root.render(<Component2 />);
+//root.render(<Component2 />);
+
+// day02 : 컴포넌트의 JSX 활용
+import Example1 from './example/2_컴포넌트/Example1.jsx';
+//root.render(<Example1 />);
+
+// day02 : 컴포넌트의 Props 활용
+import Example2 from './example/2_컴포넌트/Example2.jsx';
+//root.render(<Example2 />);
+
+// day02 : 과제1
+import Task from './example/2_컴포넌트/Task.jsx';
+root.render(<Task />);
