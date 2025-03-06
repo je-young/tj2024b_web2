dca1c5f71f81284e1004e719188f529f1e8cfa02
je-young <jey2965@gmail.com>
2025-03-06 13:24:19 +0900

    3.훅.txt 파일이동

--- src/main/react_app/src/example/3_훅/Task1.jsx
+++ src/main/react_app/src/example/3_훅/Task1.jsx
@@ -4,17 +4,16 @@
 
 import { useState } from 'react'; // useState 함수 사용
 export default function Task1() {
-  //console.log('Task1 실행'); // Task1 실행 콘솔 출력
   // Task1 컴포넌트 생성
   // (1) 여기에 state 변수 만들기
+  // const [변수명, set 변수명] = useState(초기값);
+  // - 변수명 : 아무거나.(임의)
+  // - set 변수명(새로운값)
+  // - useState(초기값) : 모든 타입의 자료 가능하다.
   const [inputvalue, setInputValue] = useState(''); // state변수 선언
-  //console.log(inputvalue); // state변수 콘솔 출력
-  //console.log(setInputValue); // state변수 콘솔 출력
 
   // input에 입력된 값이 변경될 때 실행되는 함수
   const handleChange = (event) => {
-    //console.log(event); // 이벤트 객체 콘솔 출력
-    //console.log(event.target); // 이벤트 객체의 대상 콘솔 출력
     // (2) 여기에서 state 변수값 수정하기.
     setInputValue(event.target.value); // input에 입력된 값을 state변수에 저장
     console.log(event.target.value); // input에 입력된 값 콘솔 출력
